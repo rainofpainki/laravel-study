54d1ccd8968987cd8e47422b6822d2b819728a4c
Sunha Ryu <rainofpainki@naver.com>
2017-05-31 13:16:03 +0900

    새로운 프로젝트 생성

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+// Route::get('docs/{file?}', 'DocsController@show');
+// Route::get('docs/images/{image}', 'DocsController@image')
+//       ->where('image', '[\pL-\pN\._-]+-img-[0-9]{2}.png');
+// Route::get('docs/{file?}', 'DocsController@show');
+// Route::get('docs/images/{image}', 'DocsController@image')
+//       ->where('image', '[\pL-\pN\._-]+-img-[0-9]{2}.png');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
