94f040d9118f9d86dfa885169abd1647e04c1693
Sunha Ryu <rainofpainki@naver.com>
2017-06-05 09:19:13 +0900

    강의 끝

--- app/Article.php
+++ app/Article.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $fillable = ['title', 'content'];
+  protected $fillable = ['title', 'content'];
 
-    public function user() {
-      return $this->belongsTo(User::class);
-    }
+  public function user() {
+    return $this->belongsTo(User::class);
+  }
 
-    public function tags() {
-      return $this->belongsToMany(Tag::class);
-    }
+  public function tags() {
+    return $this->belongsToMany(Tag::class);
+  }
 }
