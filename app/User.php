e986f5f14d067b73712163c4773b9e94b12b9b87
Sunha Ryu <rainofpainki@naver.com>
2017-06-05 09:21:16 +0900

    Revert "엘로퀀트 ORM"
    This reverts commit cf1b4a27e839dcb88c9326b4df2e94782c43e450.
    
    Conflicts:
    	app/Article.php
    	database/migrations/2017_06_04_123109_create_articles_table.php
    	database/migrations/2017_06_04_124252_create_tags_table.php
    	database/migrations/2017_06_04_124313_create_article_tag_table.php

--- app/User.php
+++ app/User.php
@@ -26,8 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function articles() {
-      return $this->hasMany(Article::class);
-    }
 }
